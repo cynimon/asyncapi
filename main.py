455f20e48f41204cd02e726751c0e9e7b885b0c0
AleksandraV <am.amsti@gmail.com>
2022-04-26 00:34:29 +0300

    redis connection & anagramms made

--- main.py
+++ main.py
@@ -4,10 +4,25 @@ import aioredis
 app = FastAPI()
 
 
+async def input_words(redis):
+    first = input('Input first: ')
+    second = input('Input second: ')
+    is_anagramm = False
+    if first == second[::-1]:
+        await redis.incr("counter")
+        is_anagramm = True
+    count = await redis.get("counter")
+    if count is None:
+        count = 0
+    answer = {
+        "is_anagramm": is_anagramm,
+        "counter": count
+    }
+    return answer
+
+
 @app.on_event('startup')
 async def startup_event():
-    redis = aioredis.from_url("redis://localhost")
-    await redis.set("my-key", "value")
-    value = await redis.get("my-key")
-    print(value)
-    print('okay')
+    redis = aioredis.from_url("redis://localhost", db=0, decode_responses=True)
+    result = await input_words(redis)
+    print(result)
